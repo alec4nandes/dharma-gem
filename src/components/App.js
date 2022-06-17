0cb4c9e2cedfa4d4c01d49a60bbe174c06984b0a
Alec <fernandes.alec@gmail.com>
2022-06-16 19:17:27 -0700

    add dharmaLists data

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Settings from "./Settings";
 import DharmaGem from "./DharmaGem";
+import dharmaLists from "../data/lists";
 
 export default function App() {
     const [showing, setShowing] = useState(0);
---
+++ src/data/lists.js
@@ -0,0 +1,551 @@
+const dharmaLists = {
+    "Four Noble Truths": {
+        definition: "NEEDS DEFINITION",
+        parts: [
+            {
+                definition: "The truth of suffering",
+                english: "unsatisfactoriness",
+                pali: "dukkha",
+                parts: [
+                    {
+                        definition: "birth",
+                    },
+                    {
+                        definition: "aging",
+                    },
+                    {
+                        definition: "death",
+                    },
+                    {
+                        definition: "bodily pain",
+                    },
+                    {
+                        definition: "mental pain",
+                    },
+                    {
+                        definition: "despair",
+                    },
+                    {
+                        definition: "unfulfilled desires",
+                    },
+                    {
+                        definition: "Five Aggregates",
+                    },
+                ],
+            },
+            {
+                definition:
+                    "The truth of the arising of suffering: desire or greed",
+                english: "craving or thirst",
+                pali: "tanha",
+                parts: [
+                    {
+                        definition: "sense pleasures",
+                    },
+                    {
+                        definition: "existence",
+                    },
+                    {
+                        definition: "non-existence",
+                    },
+                ],
+            },
+            {
+                definition: "The truth of the cessation of suffering",
+                parts: [
+                    {
+                        definition: "detaching from craving, desire and greed",
+                    },
+                ],
+            },
+            {
+                definition:
+                    "The truth of the path leading to the cessation of suffering",
+                parts: [
+                    {
+                        definition: "Noble Eightfold Path",
+                        english: "noble path",
+                        pali: "maggha",
+                    },
+                ],
+            },
+        ],
+    },
+    "Eightfold Path": {
+        definition: "This is the way to the ending of suffering.",
+        english: "noble path",
+        pali: "maggha",
+        parts: [
+            {
+                definition: "wisdom group",
+                english: "wisdom",
+                pali: "prajna",
+                parts: [
+                    {
+                        definition: "right understanding",
+                        english: "understading",
+                        pali: "ditthi",
+                        parts: [{ definition: "Four Noble Truths" }],
+                    },
+                    {
+                        definition: "right thought",
+                        english: "thought",
+                        pali: "sankappa",
+                        parts: [
+                            { definition: "renunciation" },
+                            { definition: "goodwill" },
+                            { definition: "not harming" },
+                        ],
+                    },
+                ],
+            },
+            {
+                definition: "morality group",
+                english: "ethical conduct",
+                pali: "sila",
+                parts: [
+                    {
+                        definition: "right speech",
+                        english: "speech",
+                        pali: "vaca",
+                        parts: [
+                            { definition: "no lying" },
+                            { definition: "no abusive speech" },
+                            { definition: "no idle chatter" },
+                        ],
+                    },
+                    {
+                        definition: "right action",
+                        english: "action",
+                        pali: "kammanta",
+                        parts: [
+                            { definition: "no killing" },
+                            { definition: "no stealing" },
+                            { definition: "no sexual misconduct" },
+                        ],
+                    },
+                    {
+                        definition: "right livelihood",
+                        english: "livelihood",
+                        pali: "ajiva",
+                        parts: [
+                            { definition: "no trading in weapons" },
+                            { definition: "no trading in human trafficking" },
+                            { definition: "no trading in meat" },
+                            { definition: "no trading in intoxicants" },
+                            { definition: "no trading in poison" },
+                        ],
+                        suttas: [
+                            {
+                                title: "Vanijja Sutta",
+                                section: "Anguttara Nikaya",
+                                verse: 5.177,
+                                link: "https://suttacentral.net/an5.177/en/sujato?layout=plain&reference=none&notes=asterisk&highlight=false&script=latin",
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                definition: "meditation group",
+                english: "concentration",
+                pali: "samadhi",
+                parts: [
+                    {
+                        definition: "right effort",
+                        english: "effort",
+                        pali: "vayama",
+                        parts: [
+                            {
+                                definition:
+                                    "abandon arisen unwholesome states of mind",
+                            },
+                            {
+                                definition:
+                                    "prevent unarisen unwholesome states of mind",
+                            },
+                            {
+                                definition:
+                                    "arouse unarisen wholesome states of mind",
+                            },
+                            {
+                                definition:
+                                    "maintain arisen wholesome states of mind",
+                            },
+                        ],
+                    },
+                    {
+                        definition: "right mindfulness",
+                        english: "mindfulness",
+                        pali: "sati",
+                        parts: [
+                            { definition: "body" },
+                            { definition: "feelings" },
+                            { definition: "general state of mind" },
+                            {
+                                definition: "Specific Mental Objects",
+                                parts: [
+                                    { definition: "Five Hinderances" },
+                                    { definition: "Five Aggregates" },
+                                    {
+                                        definition:
+                                            "Six Internal and External Spheres of Sense",
+                                    },
+                                    {
+                                        definition:
+                                            "Seven Constituents of Awakening",
+                                    },
+                                    { definition: "Four Noble Truths" },
+                                ],
+                            },
+                        ],
+                        suttas: [
+                            {
+                                title: "Satipatthana Sutta",
+                                section: "Majjhima Nikaya",
+                                verse: 10,
+                                link: "https://suttacentral.net/mn10/en/sujato?layout=plain&reference=none&notes=asterisk&highlight=false&script=latin",
+                            },
+                            {
+                                title: "Mahasatipatthana Sutta",
+                                section: "Digha Nikaya",
+                                verse: 22,
+                                link: "https://suttacentral.net/dn22/en/sujato?layout=plain&reference=none&notes=asterisk&highlight=false&script=latin",
+                            },
+                        ],
+                    },
+                    {
+                        definition: "right concentration",
+                        english: "concentration",
+                        pali: "samadhi",
+                        parts: [{ definition: "Four Meditative Absorptions" }],
+                    },
+                ],
+            },
+        ],
+    },
+    "Five Precepts": {
+        definition:
+            "Standards for lay Buddhists to live an ethical life in pursuit of enlightenment.",
+        parts: [
+            {
+                definition: "no killing",
+            },
+            {
+                definition: "no stealing",
+            },
+            {
+                definition: "no sexual misconduct",
+            },
+            {
+                definition: "no lying",
+            },
+            {
+                definition: "no taking intoxicants",
+            },
+        ],
+    },
+    "Ten Wholesome Conducts": {
+        definition:
+            "These levels of action are grouped into those of the body, speech, and mind.",
+        parts: [
+            {
+                definition: "body group",
+                parts: [
+                    {
+                        definition: "no killing",
+                    },
+                    {
+                        definition: "no stealing",
+                    },
+                    {
+                        definition: "no sexual misconduct",
+                    },
+                ],
+            },
+            {
+                definition: "speech group",
+                parts: [
+                    {
+                        definition: "no lying",
+                    },
+                    {
+                        definition: "no frivolous speech",
+                    },
+                    {
+                        definition: "no harsh words",
+                    },
+                    {
+                        definition: "no idle chatter",
+                    },
+                ],
+            },
+            {
+                definition: "mind group",
+                parts: [
+                    {
+                        definition: "no craving or lusting",
+                    },
+                    {
+                        definition: "no anger",
+                    },
+                    {
+                        definition: "no wrong views",
+                    },
+                ],
+            },
+        ],
+    },
+    "Six Virtues": {
+        definition:
+            "These are ideal qualities of Mahayana Buddhists (Theravada has 10 qualities).",
+        english: "virtue",
+        pali: "paramita",
+        parts: [
+            {
+                english: "generosity or charity",
+                pali: "dana",
+            },
+            {
+                english: "ethical conduct",
+                pali: "sila",
+            },
+            {
+                english: "patience",
+                pali: "ksanti",
+            },
+            {
+                english: "energy or determination",
+                pali: "viriya",
+            },
+            {
+                english: "meditative absorption",
+                pali: "jhana",
+            },
+            {
+                english: "wisdom",
+                pali: "prajna",
+            },
+        ],
+    },
+    "Five Aggregates": {
+        definition:
+            "Temporary conditioned phenomena that create the illusion of self.",
+        english: "aggregate",
+        pali: "skandha",
+        parts: [
+            {
+                english: "form or matter",
+                pali: "rupa",
+            },
+            {
+                english: "feelings or sensations",
+                pali: "vedana",
+            },
+            {
+                english: "perceptions",
+                pali: "samjna",
+            },
+            {
+                english: "mental formations or volition",
+                pali: "samskara",
+            },
+            {
+                english: "consciousness",
+                pali: "vijnana",
+            },
+        ],
+    },
+    "Three Poisons": {
+        definition: "The source of all unskillful actions.",
+        parts: [
+            {
+                english: "greed or attachment",
+                pali: "raga",
+            },
+            {
+                english: "anger",
+                pali: "dvesa",
+            },
+            {
+                english: "ignorance",
+                pali: "moha",
+            },
+        ],
+    },
+    "Four Attachments": {
+        definition:
+            "These are the attachments which lead to unsatisfactoriness.",
+        english: "clinging or fuel",
+        pali: "upadana",
+        parts: [
+            {
+                english: "self-doctrine clinging",
+                pali: "attavadupadana",
+            },
+            {
+                english: "wrong-view clinging",
+                pali: "ditthupadana",
+            },
+            {
+                english: "rites-and-rituals clinging",
+                pali: "silabbatupadana",
+            },
+            {
+                english: "sense-pleasure clinging",
+                pali: "kamupadana",
+            },
+        ],
+    },
+    "Six Internal and External Spheres of Sense": {
+        definition:
+            "Experiences, either pleasant, unpleasant or neutral, comes through these portals.",
+        parts: [
+            {
+                definition: "eye and seeing",
+            },
+            {
+                definition: "ear and hearing",
+            },
+            {
+                definition: "nose and smelling",
+            },
+            {
+                definition: "tongue and tasting",
+            },
+            {
+                definition: "body and touching",
+            },
+            {
+                definition: "mind and thinking",
+            },
+        ],
+    },
+    "Six Realms of Existence": {
+        definition:
+            "Analogies for conditioned experiences within samsara where beings are reborn.",
+        parts: [
+            {
+                definition: "higher experiences",
+                parts: [
+                    {
+                        defintion: "heaven",
+                    },
+                    {
+                        definition: "asura",
+                    },
+                    {
+                        definition: "human",
+                    },
+                ],
+            },
+            {
+                definition: "lower experiences",
+                parts: [
+                    {
+                        definition: "animal",
+                    },
+                    {
+                        definition: "hungry ghost",
+                    },
+                    {
+                        definition: "hell",
+                    },
+                ],
+            },
+        ],
+    },
+    "Five Hinderances": {
+        definition: "Obstacles to meditative absorption.",
+        parts: [
+            {
+                english: "sensory desire",
+                pali: "kamacchanda",
+            },
+            {
+                english: "ill-will",
+                pali: "vyapada",
+            },
+            {
+                english: "sloth-and-torpor",
+                pali: "thina-middha",
+            },
+            {
+                english: "restlessness and worry",
+                pali: "uddhacca-kukkucca",
+            },
+            {
+                english: "doubt",
+                pali: "vicikiccha",
+            },
+        ],
+    },
+    "Seven Constituents of Awakening": {
+        definition: "NEEDS DEFINITION",
+        parts: [
+            {
+                english: "mindfulness",
+                pali: "sati",
+            },
+            {
+                english: "investigation",
+                pali: "dhamma vicaya",
+            },
+            {
+                english: "energy or determination",
+                pali: "viriya",
+            },
+            {
+                english: "joy or rapture",
+                pali: "piti",
+            },
+            {
+                english: "tranquility",
+                pali: "passaddhi",
+            },
+            {
+                english: "concentration",
+                pali: "samadhi",
+            },
+            {
+                english: "equanimity",
+                pali: "uppekha",
+            },
+        ],
+    },
+    "Four Meditative Absorptions": {
+        definition: "Evolving states of meditation.",
+        english: "meditative absorption",
+        pali: "jhana",
+        parts: [
+            {
+                definition:
+                    "aloof from sense desires and unwholesome thoughts, resulting in applied and sustained thought, joy and bliss.",
+            },
+            {
+                definition:
+                    "replacing applied and sustained thought with single-pointedness of mind, tranquility, joy and bliss.",
+            },
+            {
+                definition:
+                    "replacing mental joy with equanimity, mindfulness and bodily bliss.",
+            },
+            {
+                definition:
+                    "beyond joy and bliss, suffering, and sorrow: total equanimity and mindfulness.",
+            },
+        ],
+        suttas: [
+            {
+                title: "Jhana Sutta",
+                section: "Anguttara Nikaya",
+                verse: 9.36,
+                link: "https://suttacentral.net/an9.36/en/sujato?layout=plain&reference=none&notes=asterisk&highlight=false&script=latin",
+            },
+        ],
+    },
+};
+
+console.log(dharmaLists);
+
+export default dharmaLists;
